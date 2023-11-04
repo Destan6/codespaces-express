70d12a1ac63d687c0cdbd5b9304b1fa294c60280
Destan6 <149898189+Destan6@users.noreply.github.com>
2023-11-04 18:17:17 +0000

    enis_erbay65

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+pip install pipenv
+python instagram.py -u <username> -p <passlist>
+python instagram.py --stats
+python instagram.py -px <path to proxy list>
+3.238.111.248:80
+206.189.59.192:8118
+165.22.81.30:34100
+176.248.120.70:3128
+191.242.178.209:3128
+180.92.194.235:80
+pipenv install
+pipenv --python 3.9
